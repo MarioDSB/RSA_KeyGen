c8faa0fc65dedec6b6a604c0f96e213972c0ae4e
itsmefreitas <luisffreitasp@gmail.com>
2018-11-07 01:51:51 +0000

    Added definition for generating a (possibly subject to be changed to use urandom instead of randint)!

--- genPrime.py
+++ genPrime.py
@@ -1,12 +1,12 @@
 import os
 import sys
+import random
 
-def genPrime(l):
-
+def genRand(l):
+  n=0
   w = int(l/2)
 
   p = [0]*w
-
   p[0] = 1
   p[w-1] = 1
 
@@ -17,31 +17,28 @@ def genPrime(l):
     for j in range(0,len(b)):
       p[i] = int(b[j])
 
-  print(p)
-  print(len(p))
-
-  n=0
-
-  print(w)
-
   for k in range(0,w):
     n += ((2**((w-1)-k))*p[k])
 
-  print (n)
+  return n
 
-  # print(len(p))
+def ml_composite(x):
 
-  # for i in range(1,w-2):
-  #   bitseq = os.urandom(1)
-  #   a = int(bitseq[0])
-  #   b = format(a,'08b')
-  #   for j in range(0,len(b)):
-  #     p[i] = (int(b[j]))
+  i = 1
+  y = 0
 
-  # n = 0
+  while (x % (2**i) == 0):
+    y = int(x / (2**i))
+    i+=1
 
-  # for k in range(0,w):
-  #   n += (2**k)*p[k]
+  i-=1
+
+  print(i, y)
+
+  a = random.randint(1,x)
+  print(a)
 
 if __name__ == "__main__":
-    genPrime(int(sys.argv[1]))
+    x = genRand(int(sys.argv[1]))
+    print(x)
+    ml_composite(x-1)
