332b15565b5e054f7ee4be9f2db902b1e0facb8f
itsmefreitas <luisffreitasp@gmail.com>
2018-11-07 17:51:51 +0000

    Added chained primality testing definitions!

--- genPrime.py
+++ genPrime.py
@@ -1,47 +1,141 @@
-import os
 import sys
 import random
+from datetime import datetime
 
+# Generate a random number n such that 
 def genRand(l):
   n=0
   w = int(l/2)
 
-  p = [0]*w
+  n = 0
 
-  # It's rather important to pay attention to what you're doing, since urandom returns a sequence of n BYTES...
-  bitseq = os.urandom(int(w/8))
-
-  for i in range(0,len(bitseq)):
-    b = format(bitseq[i],'08b')
-    print(b,i)
-    for j in range(0,len(b)):
-      p[i] = int(b[j])
-
-  p[0] = 1
-  p[w-1] = 1
-
-  for k in range(0,w):
-    n += ((2**((w-1)-k))*p[k])
+  while (n % 2 == 0):
+    n = random.randrange(1 << w-1, 1 << w)
 
   return n
 
-def ml_composite(x):
+# Sieve of erasthotens for the generation of the small primes up to s
+def er_sieve(s):
+  sis = [True]*(s+1)
+  
+  sis[0] = False
+  sis[1] = False
+  p = 2
 
-  i = 1
-  y = 0
+  while ((p**2) <= s):
+    if (sis[p]):
+      for i in range(p*2,s+1,p):
+        sis[i]=False
+    p += 1
+  
+  primes=[]
 
-  while (x % (2**i) == 0):
-    y = int(x / (2**i))
-    i+=1
+  for j in range(2,s):
+    if (sis[j]):
+      primes.append(j)
 
-  i-=1
+  return primes
 
-  print(i, y)
 
-  a = random.randint(1,x)
-  print(a)
+def mrab_pr(n, k):
+
+  d = n - 1
+  r = 0
+
+  while (d % 2 == 0):
+    d >>= 1
+    r += 1
+
+  for w in range(k):
+    a = random.randint(2,n-2)
+    b = pow(a,d,n)
+    if (b == 1 or b == n - 1):
+      return True
+    for i in range(r-1):
+      b = pow(b,2,n)
+      if (b == n-1):
+        break
+    
+    return False
+
+  return True
+
+def ferm_pr(n, k):
+  
+  for w in range(k):
+    a = random.randint(2,n-2)
+    b = pow(a,(n-1),n)
+    if (b != 1):
+      return False
+      
+  return True
 
 if __name__ == "__main__":
-    x = genRand(int(sys.argv[1]))
-    print(x)
-    ml_composite(x-1)
+
+  startTime = datetime.now()
+
+  p = genRand(int(sys.argv[1]))
+  q = genRand(int(sys.argv[1]))
+
+  smallPrimes = er_sieve(65537)
+
+  # TODO: fixme make this loop less loopy (if you know what I mean); this code is UGLY.
+
+  while (True):
+    p = genRand(int(sys.argv[1]))
+    
+    # Using sieve of erasthotens to eliminate numbers that are divisible by the first 5000 known primes.
+    while (any( p % i == 0 for i in smallPrimes)):
+      p = genRand(int(sys.argv[1]))
+
+    # If the number passed the previously (very) naive test, then do checks with Fermat's little theorem.
+    while (not ferm_pr(p,10)):
+      p = genRand(int(sys.argv[1]))
+      while (any( p % i == 0 for i in smallPrimes)):
+        p = genRand(int(sys.argv[1]))
+    
+    # If we've got a survivor here (highly unlikely), perform the more computationally complex miller-rabin test.
+    while (not mrab_pr(p,10)):
+      p = genRand(int(sys.argv[1]))
+      # Sadly we have to recheck all the previous conditions after generating a new number...
+      while (any( p % i == 0 for i in smallPrimes)):
+        p = genRand(int(sys.argv[1]))
+      
+      while (not ferm_pr(p,10)):
+        p = genRand(int(sys.argv[1]))
+        while (any( p % i == 0 for i in smallPrimes)):
+          p = genRand(int(sys.argv[1]))
+    
+    break
+
+  while (True):
+    q = genRand(int(sys.argv[1]))
+    
+    # Using sieve of erasthotens to eliminate numbers that are divisible by the first 5000 known primes.
+    while (any( q % i == 0 for i in smallPrimes)):
+      q = genRand(int(sys.argv[1]))
+
+    # If the number passed the previously (very) naive test, then do checks with Fermat's little theorem.
+    while (not ferm_pr(q,10)):
+      q = genRand(int(sys.argv[1]))
+      while (any( q % i == 0 for i in smallPrimes)):
+        q = genRand(int(sys.argv[1]))
+    
+    # If we've got a survivor here (highly unlikely), perform the more computationally complex miller-rabin test.
+    while (not mrab_pr(q,10)):
+      q = genRand(int(sys.argv[1]))
+      # Sadly we have to recheck all the previous conditions after generating a new number...
+      while (any( q % i == 0 for i in smallPrimes)):
+        q = genRand(int(sys.argv[1]))
+      
+      while (not ferm_pr(q,10)):
+        q = genRand(int(sys.argv[1]))
+        while (any( q % i == 0 for i in smallPrimes)):
+          q = genRand(int(sys.argv[1]))
+    
+    break
+
+  print(p)
+  print(q)
+  print("")
+  print(datetime.now() - startTime)
