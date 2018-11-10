1ad1b552833d8da81d36b3ae9a46eabd6ad71b20
itsmefreitas <luisffreitasp@gmail.com>
2018-11-10 22:56:29 +0000

    Added enforcement for RSA-safe (strong) primes, don't forget to study the sources to include in the report.

--- genPrime.py
+++ genPrime.py
@@ -2,10 +2,13 @@ import sys
 import random
 from datetime import datetime
 
+l = int(sys.argv[1])
 
-max_rounds = 10
+max_rounds = 100
 smallPrimes = []
 e = 65537
+# nsieve such that this sieve will have the first n small primes.
+nsieve = 7920
 
 
 # Generate a random number n such that
@@ -53,70 +56,28 @@ def miller_rabin_pt(n, rounds):
         d >>= 1
         r += 1
 
-    for w in range(rounds):
+    for _ in range(rounds):
         a = random.randint(2, n-2)
         b = pow(a, d, n)
         if b == 1 or b == n - 1:
-            return True
-        for i in range(r-1):
+            continue
+        for _ in range(r-1):
             b = pow(b, 2, n)
             if b == n-1:
                 break
-
-        return False
-
-    return True
-
-
-def fermat_lt(n, rounds):
-
-    for w in range(rounds):
-        a = random.randint(2, n-2)
-        b = pow(a, (n-1), n)
-        if b != 1:
+        else:
             return False
 
     return True
 
 
-def fermat_test(n):
-    while not fermat_lt(n, max_rounds):
-        n = gen_rand(int(sys.argv[1]))
-
-        while any(n % i == 0 and n != i for i in smallPrimes):
-            n = gen_rand(int(sys.argv[1]))
-
-    return n
-
-
-def miller_rabin_test(n):
-    while not miller_rabin_pt(n, max_rounds):
-        n = gen_rand(int(sys.argv[1]))
-
-        # We have to recheck all the previous conditions after generating a new number
-        while any(n % i == 0 and n != i for i in smallPrimes):
-            n = gen_rand(int(sys.argv[1]))
-
-        n = fermat_test(n)
-
-    return n
-
-
 def generate_prime(n):
-    while not miller_rabin_pt(2*n + 1, max_rounds) and not miller_rabin_pt(n, max_rounds):
-        n = gen_rand(int(sys.argv[1]))
+    while not miller_rabin_pt(n, max_rounds):
+        n = gen_rand(l)
 
         # Using sieve of Erasthotens to eliminate numbers that are divisible by the first X known primes.
-        while any(n % i == 0 and n != i for i in smallPrimes):
-            n = gen_rand(int(sys.argv[1]))
-
-        # If the number passed the previously (very) naive test, do checks with Fermat's little theorem.
-        n = fermat_test(n)
-
-        # If we've got a survivor here (highly unlikely), perform the more computationally complex miller-rabin test.
-        n = miller_rabin_test(n)
-
-        break
+        while any(n % i == 0 and n > i for i in smallPrimes):
+            n = gen_rand(l)
 
     return n
 
@@ -136,21 +97,49 @@ def gcd(a, b):
 if __name__ == "__main__":
     startTime = datetime.now()
 
-    p = gen_rand(int(sys.argv[1]))
-    q = gen_rand(int(sys.argv[1]))
+    p = gen_rand(l)
+    q = gen_rand(l)
 
-    smallPrimes = er_sieve(e)
+    smallPrimes = er_sieve(nsieve)
 
     p = generate_prime(p)
     q = generate_prime(q)
+
+    # p and q can never be ==, factorization would be too easy, i.e. n would be a perfect square.
+
+    n = p*q
+
+    exp = int(l/3)+1
+
+    print (exp)
+
+    print(len(bin(n))-2)
+
+
+    # Primes are RSA-SAFE if their absolute value is equal or greater than their safe distance.
+    # Alternatively, a prime p is a strong prime if (p-1) has a very large prime factor
+    # Source: https://eprint.iacr.org/2009/318.pdf
+    # Source: https://crypto.stackexchange.com/questions/5262/rsa-and-prime-difference
+    safedist = pow(2,exp)
+
+    print(safedist)
+
+    while p == q or abs(p-q) < safedist:
+        q = generate_prime(l)
 
     while not gcd(e, (p-1)*(q-1)) == 1:
         p = generate_prime(p)
         q = generate_prime(q)
 
+    #print(abs(p-q) >= safedist)
+
+    #print (p)
+    #print ("")
+    #print (q)
+
     # We need to calculate d (using Extended Euclid)
-    print(p*q, p, q, e, )
-    print("")
+    #print(p*q, p, q, e, )
+    #print("")
 
     # No need to use this timer. We can just run "time python genPrime 4096"
     t = datetime.now() - startTime
--- runtimeDistr.py
+++ runtimeDistr.py
@@ -11,6 +11,11 @@ if __name__ == "__main__":
   t=[]
 
   for i in range(int(sys.argv[2])):
-    t.append(getTime(sys.argv[1]))
+    k = float(str(getTime(sys.argv[1]))[7:16])
+    t.append(k)
 
+
+  avg = sum(t)/len(t)
+  
   print (t)
+  print (avg)
