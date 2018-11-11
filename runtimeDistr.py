0e8ad0d2d1c5f3dd8b5f83682854183cfe945d85
itsmefreitas <luisffreitasp@gmail.com>
2018-11-11 20:04:45 +0000

    The runtime distribution histogram applet is now on steroids.

--- runtimeDistr.py
+++ runtimeDistr.py
@@ -1,17 +1,52 @@
+from datetime import datetime
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+import numpy as np
+import math
 import subprocess
 import sys
 
-
-def get_time(l):
-    out = subprocess.check_output('python3 genPrime.py ' + str(l), stderr=subprocess.STDOUT, shell=True)
-    return out
-
+def getTime(l):
+  startTime = datetime.now()
+  out = subprocess.check_output('python3 genPrime.py '+str(l),stderr=subprocess.STDOUT,shell=True)
+  t = datetime.now() - startTime
+  return str(t)
 
 if __name__ == "__main__":
+  
+  t=[]
 
-    t = []
+  # Run prime generation for the specified number of times and register times.
+  for i in range(int(sys.argv[2])):
+    k = float(str(getTime(sys.argv[1]))[5:15])
+    t.append(k)
 
-    for i in range(int(sys.argv[2])):
-        t.append(get_time(sys.argv[1]))
+  # Check the average runtime
+  avg = sum(t)/len(t)
+  
+  print (t)
+  print (avg)
 
-    print(t)
+  it = []
+
+  # Round the values of t list of times to the nearest integer value for seconds.
+  for i in range(math.floor(min(t)),math.ceil(max(t))+1,1):
+    count_i=0
+    for j in t:
+      if (round(j) == i):
+        count_i+=1
+    it.append((i,count_i))
+
+  # Frequency for k seconds
+  kf = [x[1] for x in it]
+  # Value of k seconds rounded to nearest int
+  kv = [x[0] for x in it]
+
+  # Save graph with the name below + number of times ran.
+  figname="LPrimeFreqs"+str(sys.argv[2])
+
+  indices = np.arange(len(it))
+  plt.bar(indices,kf,color='b')
+  plt.xticks(indices,kv,rotation='vertical')
+  plt.tight_layout()
+  plt.savefig(figname,pad_inches=0.1)
