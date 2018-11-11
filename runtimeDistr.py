65efddef74d4c5319a6140a65a1fe9aa5cbffca4
MarioDSB <mariodinis1997@gmail.com>
2018-11-11 18:39:58 +0000

    Merge branch 'master' of /home/mario/eclipse-workspace/Crypto_Trab1/Crypto_Trab1 with conflicts.

--- runtimeDistr.py
+++ runtimeDistr.py
@@ -1,39 +1,16 @@
 from datetime import datetime
-import matplotlib.pyplot as plt
-import scipy.stats as stats
-import numpy as np
-import math
 import subprocess
 import sys
 
 def getTime(l):
-  startTime = datetime.now()
   out = subprocess.check_output('python3 genPrime.py '+str(l),stderr=subprocess.STDOUT,shell=True)
-  t = datetime.now() - startTime
-  return str(t)
+  return out
 
 if __name__ == "__main__":
   
   t=[]
-  b=[]
 
   for i in range(int(sys.argv[2])):
-    k = float(str(getTime(sys.argv[1]))[5:15])
-    if (int(k) not in b):
-      b.append(int(k))
-    t.append(k)
+    t.append(getTime(sys.argv[1]))
 
-
-  avg = sum(t)/len(t)
-  
   print (t)
-  print (avg)
-
-  bns = list(range(0,math.ceil(max(t))+1,1))
-
-  figname="LPrimeFreqs"+str(sys.argv[2])
-
-  plt.xticks(bns)
-  plt.yticks(list(range(0,int(sys.argv[2])+1,1)))
-  plt.hist(t, bins=bns)
-  plt.savefig(figname,pad_inches=0.1)
